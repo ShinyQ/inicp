db3105ca71b5134deb0be3ff200caae6818585ae
alfi2811 <alfinasution64@gmail.com>
2020-02-07 19:42:03 +0700

    first

---
+++ soal1/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+// Complete the compareTriplets function below.
+func compareTriplets(a []int32, b []int32) []int32 {
+    var i int    
+    var newest []int32
+    newest = append(newest, 0)
+    newest = append(newest, 0)
+    i = 0
+    for i < len(a) && i < len(b) {
+        if a[i] > b[i] {
+            newest[0] = newest[0] + 1            
+
+        } else if b[i] > a[i] {
+            newest[1] = newest[1] + 1
+        }
+        i++
+    }
+
+    return newest
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    aTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var a []int32
+
+    for i := 0; i < 3; i++ {
+        aItemTemp, err := strconv.ParseInt(aTemp[i], 10, 64)
+        checkError(err)
+        aItem := int32(aItemTemp)
+        a = append(a, aItem)
+    }
+
+    bTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var b []int32
+
+    for i := 0; i < 3; i++ {
+        bItemTemp, err := strconv.ParseInt(bTemp[i], 10, 64)
+        checkError(err)
+        bItem := int32(bItemTemp)
+        b = append(b, bItem)
+    }
+
+    result := compareTriplets(a, b)
+
+    for i, resultItem := range result {
+        fmt.Fprintf(writer, "%d", resultItem)
+
+        if i != len(result) - 1 {
+            fmt.Fprintf(writer, " ")
+        }
+    }
+
+    fmt.Fprintf(writer, "\n")
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
